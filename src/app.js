8d23a87a871fdb8f7471b3f9da1087307f9d1034
julietacasano <julicasano2626@gmail.com>
2026-05-27 11:08:10 -0300

    Primer version hola mundo

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hola mundo desde Integracion Continua"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok"
+  });
+});
+
+module.exports = app;

4613f88cf0cc9ec863c3fd4a4c5140433d183a23
julietacasano <julicasano2626@gmail.com>
2026-05-27 15:55:53 -0300

    Agregar pipeline de integracion continua

---
+++ tests/app.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("GET /health", () => {
+  test("Debe responder status ok", async () => {
+    const response = await request(app).get("/health");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      status: "ok"
+    });
+  });
+});
